50a9bfc09d5a66a47969f862f4079ffd259479e6
Dennis Vashchuk <dennis.vashchuk@gmail.com>
2021-09-18 10:38:15 +0300

    Add map-id

---
+++ install-dev.sh
@@ -0,0 +1,7 @@
+#!/bin/sh -xe
+install -d ~/.local/bin
+ln -snf $(readlink -e bin/open) ~/.local/bin/box-open
+ln -snf $(readlink -e bin/share-folder) ~/.local/bin/box-share-folder
+ln -snf $(readlink -e bin/share-usb) ~/.local/bin/box-share-usb
+ln -snf $(readlink -e bin/launch) ~/.local/bin/box-launch
+ln -snf $(readlink -e bin/map-id) ~/.local/bin/box-map-id
